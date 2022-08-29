1219e1de272b0a12741404934616640a7017aa88
Naima Duyser <naima.fatimaduyser@gmail.com>
2022-08-29 14:44:38 +0200

    changes

--- src/components/cards.js
+++ src/components/cards.js
@@ -39,7 +39,8 @@ function Cards() {
       maindata.audioPlayChange
         ? setplayindex(maindata.audioIndex)
         : setplayindex(-1);
-    }
+    },
+    [maindata.audioPlayChange, maindata.audioIndex]
   );
 
   // if (data) console.log(data.mixes);
