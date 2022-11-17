91083741ab6880e56a0f0fcaf1e8209306bcdb6d
Naima <naima@xel.nl>
2022-11-17 11:19:43 +0100

    quick changes, removed navlinks for now, margin fixes

--- src/components/scripts/Navbar.js
+++ src/components/scripts/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState } from 'react'
+// import { useState } from 'react'
 import { NavLink } from "react-router-dom"
 
 // Styling
@@ -9,20 +9,20 @@ import "../styles/navbar.css"
 import earth from "../../assets/images/earth.png"
 
 const Navbar = () => {
-    const [click, setClick] = useState(false);
-    const handleClick = () => setClick(!click);
-    // const closeSideBar = () => {
-    //     setClick(false);
-    // }
-    console.log(window.screen.width);
+  // const [click, setClick] = useState(false);
+  // const handleClick = () => setClick(!click);
+  // const closeSideBar = () => {
+  //     setClick(false);
+  // }
+  console.log(window.screen.width);
 
-    return (
-        <div className="header">
-          <div className="header-logo">
-                <NavLink to="/" className="logo">naima.world</NavLink>
-          </div>
+  return (
+    <div className="header">
+      <div className="header-logo">
+        <NavLink to="/" className="logo">naima.world</NavLink>
+      </div>
 
-          <div className="navmenu-container">
+      {/* <div className="navmenu-container">
             <ul className={click ? 'nav-menu active' : 'nav-menu'}>
               <li className="navlink-list-item" onClick={window.screen.width <= "1240" && handleClick}>
                   <NavLink to="/residents" className="navlink">residents</NavLink>
@@ -31,13 +31,17 @@ const Navbar = () => {
                   <NavLink to="/contact" className="navlink">contact</NavLink>
               </li>
             </ul> 
-          </div>
+          </div> 
 
           <div className="header-image" onClick={handleClick}>
             <img src={earth} alt="earth illustration" className="earth"/>
-          </div>
-        </div>
-    )
+          </div> */}
+
+      <div className="header-image" >
+        <img src={earth} alt="earth illustration" className="earth" />
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
--- src/routes/Resident.js
+++ src/routes/Resident.js
@@ -1,16 +1,35 @@
 import React from "react";
+import { graphql } from "react-apollo";
+import {gql} from '@apollo/client'
 
-
-// Styling
-import "../components/styles/resident.css"
-
-
-const Resident = (props) => {
-    return (
+const Resident = ({data: { loading, resident } }) => {
+    if (!loading) {
+        return (
             <div className="container">
-               test
+                <h1>{resident.name}</h1>
+                <img src={resident.image.url} alt="resident" className="resident-image" />
             </div>
-        );
-    };
+        )
+    }
+    return <h2>Loading....</h2>
+}
 
-    export default Resident;
+const singleResident = gql`
+    query singleResident($slug: String!) {
+        resident: Resident(slug: $slug) {
+            name
+            image {
+                url
+            }
+            biography
+        }
+    }
+`
+
+export default graphql(singleResident, {
+    options: ({ match }) => ({
+        variables: {
+            slug: match.params.slug
+        }
+    })
+})(Resident);
